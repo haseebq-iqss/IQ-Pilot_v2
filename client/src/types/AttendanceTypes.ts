5e5e6fff122f61a9850e07c422aaa69ac818fde7
Sheikh Umair <umair.najeeb@iquasar.com>
2024-03-28 16:01:50 +0530

    FEAT : Added User And Attendance Types

---
+++ client/src/types/AttendanceTypes.ts
@@ -0,0 +1,9 @@
+type AttendanceTypes = {
+  id?: string;
+  ofEmployee: string;
+  ofRoute: string;
+  isPresent: boolean;
+  createdAt?: Date;
+};
+
+export default AttendanceTypes;
--- client/src/types/EmployeeTypes.ts
+++ client/src/types/EmployeeTypes.ts
@@ -1,5 +1,21 @@
 type EmployeeTypes = {
-    name : string,
-}
+  id?: string;
+  fName: string;
+  lName: string;
+  phone: number;
+  email: string;
+  role: "admin" | "employee" | "driver";
+  password: string;
+  profilePicture: string;
+  department: string;
+  pickup: [number, number];
+  address: string;
+  currentShift: string;
+  workLocation: "Rangreth" | "Zaira Tower";
+  isCabCancelled: boolean;
+  leaveType: "pickup" | "drop" | "both";
+  startTime: number;
+  createdAt?: Date;
+};
 
-export default EmployeeTypes
+export default EmployeeTypes;
