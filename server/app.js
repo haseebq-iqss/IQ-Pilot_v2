bc3b200d72bf6528b48e880cf87c004e664ae234
AbdulAzeemBeigh <abdul.azeem@iquasar.com>
2024-04-02 06:56:57 -0400

    FEAT:Handled cors and cookie

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const globalErrorController = require("./controller/errorController");
 const express = require("express");
 const AppError = require("./utils/appError");
+const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
+app.use(cors(["http://localhost:5173/", "http://localhost:5174/"]));
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/v2/auth", authRoutes);
