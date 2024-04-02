4483847a9a1ad4308a609db151a8b4ef6595a379
Arshiya-iqss <arshiya.shakeel@iquasar.com>
2024-04-02 15:26:06 +0530

    cabTypes added

---
+++ client/src/types/CabTypes.ts
@@ -0,0 +1,11 @@
+type CabTypes = {
+  id?: string;
+  cabDriver: string;
+  cabNumber: string;
+  seatingCapacity: number;
+  numberPlate: string;
+  carModel?: string;
+  carColor?: string;
+};
+
+export default CabTypes;
