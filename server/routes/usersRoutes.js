1014f7f56865a5c97e6a69b5890214350033d1cf
AbdulAzeemBeigh <abdul.azeem@iquasar.com>
2024-04-09 05:13:59 -0400

    Changes in user routes

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,36 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const { protect, restrictTo } = require("../controller/authController");
-// const {
-//   getAllEmployees,
-//   getAllTeamMembers,
-//   getAllDrivers,
-//   getTm,
-//   getDriver,
-//   updateUser,
-//   deleteUser,
-// } = require("../controller/employeeController");
-
-// router.use(protect);
-
-// router.get("/", restrictTo("admin"), getAllEmployees);
-// router.get("/tms", restrictTo("admin"), getAllTeamMembers);
-// router.get("/drivers", restrictTo("admin"), getAllDrivers);
-
-// router
-//   .route("/tm/:id")
-//   .get(restrictTo("admin", "employee"), getTm)
-//   .patch(restrictTo("admin", "employee"), updateUser)
-//   .delete(restrictTo("admin"), deleteUser);
-
-// router
-//   .route("/driver/:id")
-//   .get(restrictTo("admin", "driver"), getDriver)
-//   .patch(restrictTo("admin", "driver"), updateUser)
-//   .delete(restrictTo("admin"), deleteUser);
-
-// module.exports = router;
 const express = require("express");
 const upload_profile_pic = require("../controller/uploadProfilePicController");
 
