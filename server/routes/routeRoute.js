cb7779e93dcd31bf48f7a8ed59b72830a0d584eb
SaaibQ <saaib.qazi@iquasar.com>
2024-04-29 17:58:04 +0530

    Route Controller implemented pendingPassengers,rosteredPassengers and get driver route

--- server/routes/routeRoute.js
+++ server/routes/routeRoute.js
@@ -5,12 +5,18 @@ const {
   createRoute,
   getRoutes,
   getRoute,
+  pendingPassengers,
+  rosteredPassengers,
+  driverRoute,
 } = require("../controller/routeController");
 const { protect, restrictTo } = require("../controller/authController");
 
 router.use(protect);
 router.use(restrictTo("admin"));
 
+router.route("/pendingPassengers").get(pendingPassengers);
+router.route("/rosteredPassengers").get(rosteredPassengers);
+router.route("/driverRoute/:id").get(driverRoute);
 router.route("/shifts").post(createShift);
 router.route("/").post(createRoute).get(getRoutes);
 router.route("/:id").get(getRoute);
