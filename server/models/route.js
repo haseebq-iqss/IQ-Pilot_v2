e0eca77ea639e63223383c96018d0c49bdc281c2
SaaibQ <saaib.qazi@iquasar.com>
2024-05-06 16:01:19 +0530

    Updated route model.

--- server/models/route.js
+++ server/models/route.js
@@ -1,55 +1,57 @@
 const mongoose = require("mongoose");
 
-const routeSchema = new mongoose.Schema(
-  {
-    cab: {
+const routeSchema = new mongoose.Schema({
+  cab: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Cab",
+    required: true,
+  },
+  workLocation: {
+    type: String,
+    enum: ["Zaira Tower", "Rangreth"],
+  },
+  currentShift: String,
+  passengers: [
+    {
       type: mongoose.Schema.ObjectId,
-      ref: "Cab",
+      ref: "User",
       required: true,
     },
-    workLocation: {
-      type: String,
-      enum: ["Zaira Tower", "Rangreth"],
-    },
-    currentShift: String,
-    passengers: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
+  ],
+  shiftDate: {
+    type: String,
+  },
+  typeOfRoute: {
+    type: String,
+    enum: ["pickup", "drop", "supply"],
+    required: true,
+  },
+  routeStatus: {
+    type: String,
+    enum: ["notStarted", "inProgress", "completed"],
+    default: "notStarted",
+  },
+  estimatedTime: {
+    type: Number,
+  },
+  cabPath: [
+    {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
       },
-    ],
-    shiftDate: {
-      type: String,
+      // LonLat
+      coordinates: [Number],
+      description: String,
     },
-    typefRoute: {
-      type: String,
-      enum: ["pickup", "drop", "supply"],
-      required: true,
-    },
-    routeStatus: {
-      type: String,
-      enum: ["notStarted", "inProgress", "completed"],
-      default: "notStarted",
-    },
-    estimatedTime: {
-      type: Number,
-    },
-    cabPath: [
-      {
-        type: {
-          type: String,
-          default: "Point",
-          enum: ["Point"],
-        },
-        // LonLat
-        coordinates: [Number],
-        description: String,
-      },
-    ],
-    totalDistance: {
-      type: Number,
-    },
+  ],
+  totalDistance: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
