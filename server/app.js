d05db67f74cffee3c74f8f4a89f3b66f93897a39
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-06-04 16:48:50 +0530

    TS Check +++

--- server/app.js
+++ server/app.js
@@ -22,10 +22,11 @@ app.use(function (req, res, next) {
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173"],
     exposedHeaders: "Set-Cookie",
   })
 );
+
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
