ec735bb8ce8bfa2addd01de2fec82f381e2ba79a
SaaibQ <saaib.qazi@iquasar.com>
2024-06-10 17:31:03 +0530

    Stats to get distance covered for a month and shrinkage % for a month

--- server/routes/attendanceRoutes.js
+++ server/routes/attendanceRoutes.js
@@ -6,19 +6,12 @@ const { catchAsync } = require("../utils/catchAsync");
 
 const router = express.Router();
 
-
 router.use(protect);
-router.use(restrictTo("admin", "driver"))
+router.use(restrictTo("admin", "driver"));
 
-router.post(
-  "/",
-  attendanceController.createAttendance
-);
-
-router.get(
-  "/",
-  attendanceController.getAllAttendances
-);
+router.post("/", attendanceController.createAttendance);
+router.get("/", attendanceController.getAllAttendances);
+router.get("/shrinkageMonth", attendanceController.getMonthShrinkage);
 
 router
   .route("/:id")
@@ -26,6 +19,6 @@ router
   .put(attendanceController.updateAttendance)
   .delete(attendanceController.deleteAttendance);
 
-router.get("/route-attendance/:id", attendanceController.getAttendanceByRoute)
+router.get("/route-attendance/:id", attendanceController.getAttendanceByRoute);
 
 module.exports = router;
