518089c9b4f53e7bbb9dbddb6ab47017600eea2e
SaaibQ <saaib.qazi@iquasar.com>
2024-07-01 19:26:20 +0530

    Changes in the cab available api, no of cabs available logic changed

--- server/controller/cabController.js
+++ server/controller/cabController.js
@@ -140,67 +140,111 @@ exports.getTMSAssignedCabs = catchAsync(async (req, res, next) => {
 
 exports.availableCabs = catchAsync(async (req, res, next) => {
   const allRoutes = await Route.find({});
-  const allCabs = await Cab.find({}).populate("cabDriver");
+  // const allCabs = await Cab.find({}).populate("cabDriver");
+  const cabs = await Cab.aggregate([
+    {
+      $lookup: {
+        from: "routes",
+        foreignField: "cab",
+        localField: "_id",
+        as: "routes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "cabDriver",
+        as: "cabDriver",
+      },
+    },
+  ]);
+
+  // NOT TO CONSIDER NON-ACTIVE ROUTES ASSIGNED TO CABS(FILTERING OF CABS)
+  const present_day = new Date();
+  present_day.setHours(0, 0, 0, 0);
+  for (const cab of cabs) {
+    cab.routes = cab.routes
+      .map((route) => {
+        const route_created = new Date(route.createdAt);
+        route_created.setHours(0, 0, 0, 0);
+        const end_date = new Date(route.createdAt);
+        end_date.setDate(route_created.getDate() + route.daysRouteIsActive);
+        end_date.setHours(0, 0, 0, 0);
+        if (
+          route_created.getTime() < present_day.getTime() &&
+          end_date.getTime() < present_day.getTime()
+        )
+          return null;
+        return route;
+      })
+      .filter((val) => val !== null);
+  }
 
   const activeRoutes = await activeRoutesFun(allRoutes);
 
-  const activeRoutesNoCapacity = activeRoutes.filter(
-    (route) => route.availableCapacity === 0
-  );
+  // const activeRoutesAssignedCab = activeRoutes.filter(
+  //   (route) => route.availableCapacity === 0
+  // );
 
-  const assignedCabsWithCapacity = await Promise.all(
-    activeRoutes.map(async (route) => {
-      if (route.availableCapacity > 0) {
-        const cab = await Cab.findById(route.cab).populate("cabDriver");
-        return {
-          _id: cab._id,
-          cabDriver: cab.cabDriver,
-          seatingCapacity: route.availableCapacity,
-          cabNumber: cab.cabNumber,
-          numberPlate: cab.numberPlate,
-          carColor: cab.carColor,
-          carModel: cab.carModel,
-          passengers: [...route.passengers],
-        };
-      }
-      return null;
-    })
-  );
+  const activeRoutesAssignedCab = activeRoutes.map((route) => route.cab);
+  // const assignedCabsWithCapacity = await Promise.all(
+  //   activeRoutes.map(async (route) => {
+  //     if (route.availableCapacity > 0) {
+  //       const cab = await Cab.findById(route.cab).populate("cabDriver");
+  //       return {
+  //         _id: cab._id,
+  //         cabDriver: cab.cabDriver,
+  //         seatingCapacity: route.availableCapacity,
+  //         cabNumber: cab.cabNumber,
+  //         numberPlate: cab.numberPlate,
+  //         carColor: cab.carColor,
+  //         carModel: cab.carModel,
+  //         passengers: [...route.passengers],
+  //       };
+  //     }
+  //     return null;
+  //   })
+  // );
 
-  const availableAssignedCabs = assignedCabsWithCapacity.filter(
-    (val) => val !== null
-  );
+  // const availableAssignedCabs = assignedCabsWithCapacity.filter(
+  //   (val) => val !== null
+  // );
 
-  const cabsNotAvailable = activeRoutesNoCapacity.map((route) =>
-    route.cab.toString()
-  );
+  // const cabsNotAvailable = activeRoutesNoCapacity.map((route) =>
+  //   route.cab.toString()
+  // );
 
-  const noOfCabsAvailable = allCabs.filter(
+  const noOfCabsAvailable = cabs.filter(
     (cab) =>
-      !cabsNotAvailable.includes(cab._id.toString()) &&
-      !availableAssignedCabs.some((assignedCab) => {
-        return (
-          assignedCab.cabDriver._id.toString() === cab.cabDriver._id.toString()
-        );
-      })
+      !activeRoutesAssignedCab.includes(cab._id.toString()) &&
+      cab.routes.length !== 2
   );
 
-  const cabsAvailable = noOfCabsAvailable.map((cab) => {
-    return {
-      _id: cab._id,
-      cabDriver: cab.cabDriver,
-      cabNumber: cab.cabNumber,
-      numberPlate: cab.numberPlate,
-      carColor: cab.carColor,
-      carModel: cab.carModel,
-      seatingCapacity: cab.seatingCapacity,
-      passengers: [],
-    };
-  });
+  // const cabsAvailable = noOfCabsAvailable.map((cab) => {
+  //   return {
+  //     _id: cab._id,
+  //     cabDriver: cab.cabDriver,
+  //     cabNumber: cab.cabNumber,
+  //     numberPlate: cab.numberPlate,
+  //     carColor: cab.carColor,
+  //     carModel: cab.carModel,
+  //     seatingCapacity: cab.seatingCapacity,
+  //     passengers: [],
+  //   };
+  // });
 
-  res.status(200).json({
-    status: "Success",
-    results: [...cabsAvailable].length,
-    // data: [...cabsAvailable, ...availableAssignedCabs],
-  });
+  // res.status(200).json({
+  //   status: "Success",
+  //   results: [...cabsAvailable].length,
+  //   data: [...cabsAvailable],
+  // });
+  // console.log(noOfCabsAvailable.length);
+  res
+    .status(200)
+    .json({
+      status: "Success",
+      result: noOfCabsAvailable.length,
+      cabsAvailable: noOfCabsAvailable,
+    });
 });
--- server/controller/errorController.js
+++ server/controller/errorController.js
@@ -41,7 +41,7 @@ function sendProdError(err, res) {
 }
 
 module.exports = (err, req, res, next) => {
-  // console.log(err);
+  console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
