1a41fa7b3b6464ce2b183a4f547fc4738e8c2c8c
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-07-02 14:56:05 +0530

    new server added +++

--- client/src/utils/baseURL.ts
+++ client/src/utils/baseURL.ts
@@ -1,5 +1,6 @@
 // const baseURL = "http://localhost:3000/";
-const baseURL = "https://iqv2-be.onrender.com/";
+// const baseURL = "https://iqv2-be.onrender.com/";  // SERVER DEAD
+const baseURL = "https://iq-pilot-v2.onrender.com/";
 // const baseURL = "https://6zkcx3p4-3000.inc1.devtunnels.ms/";
 
 export default baseURL;
