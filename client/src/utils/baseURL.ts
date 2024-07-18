66e7f4aceeb432ab27cd351c7875ea2fcc44fa8e
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-07-18 16:23:04 +0530

    baseURL changed +++

--- client/src/utils/baseURL.ts
+++ client/src/utils/baseURL.ts
@@ -1,6 +1,7 @@
 // const baseURL = "http://localhost:3000/";
 // const baseURL = "https://iqv2-be.onrender.com/";  // SERVER DEAD
-const baseURL = "https://iq-pilot-v2.onrender.com/";
+// const baseURL = "https://iq-pilot-v2.onrender.com/";
+const baseURL = "http://iquasarpilot-env.eba-pbwmqjni.ap-south-1.elasticbeanstalk.com/"     //AWS Deployment;
 // const baseURL = "https://6zkcx3p4-3000.inc1.devtunnels.ms/";
 
 export default baseURL;
