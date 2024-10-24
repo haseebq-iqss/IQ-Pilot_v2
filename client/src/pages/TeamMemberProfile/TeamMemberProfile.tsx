5efc512cb0c772718600746d1685801327198562
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-10-24 17:28:37 +0530

    type fix +++

--- client/src/pages/TeamMemberProfile/TeamMemberProfile.tsx
+++ client/src/pages/TeamMemberProfile/TeamMemberProfile.tsx
@@ -1,8 +1,6 @@
-import { PageFlex } from "../../style_extentions/Flex"
-
 function TeamMemberProfile() {
   return (
-    <div style={{fontSize:"10rem", ...PageFlex, backgroundColor:"red"}}>TeamMemberProfile</div>
+    <div style={{fontSize:"10rem"}}>TeamMemberProfile</div>
   )
 }
 
